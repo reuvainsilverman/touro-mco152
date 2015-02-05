2d99be5c79410ae5c726dadfcd724db7cd149065
eytansterling <eytansterling@gmail.com>
2015-02-04 20:10:43 -0500

    This is a test method that outputs the product of 8 and 9.

--- Chess/src/edu/touro/mco152/EtyansClass.java
+++ Chess/src/edu/touro/mco152/EtyansClass.java
@@ -1,5 +1,12 @@
 package edu.touro.mco152;
 
-public class EtyansClass {
 
+public class EtyansClass {
+	public void testing() {
+	int first=8, second=9;
+	System.out.print(first*second);
 }
+	
+	
+}
+
